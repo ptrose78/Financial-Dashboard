a520a03a3ecd030abbfb7feabbd1f25c1dc0966c
Paul Rose <ptrose78@gmail.com>
2024-12-27 12:17:16 -0600

    Load data from postgres database and display on dashboard.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,12 +14,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -76,7 +76,7 @@ export async function fetchCardData() {
       numberOfInvoices,
       totalPaidInvoices,
       totalPendingInvoices,
-    };
+    };  
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch card data.');
